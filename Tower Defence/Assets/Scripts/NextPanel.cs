7d5c0e654abed2d94ca81099ed656fc99e009040
lusia8500 <43332486+lusia8500@users.noreply.github.com>
2019-10-23 14:38:44 +0900

    2019.10.23
    TowerDefense

---
+++ Tower Defence/Assets/Scripts/NextPanel.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NextPanel : MonoBehaviour
+{
+
+    public GameObject Panel1;
+    public GameObject Panel2;
+    public GameObject Panel3;
+    public GameObject Panel4;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Panel1.SetActive(false);
+        Panel2.SetActive(false);
+        Panel3.SetActive(false);
+        Panel4.SetActive(false);
+    }
+
+    public void Trigger1()
+    {
+        Panel2.SetActive(true);
+    }
+
+    public void Trigger2()
+    {
+        Panel2.SetActive(true);
+    }
+
+    public void Trigger3()
+    {
+        Panel3.SetActive(true);
+    }
+
+    public void Trigger4()
+    {
+        Panel4.SetActive(true);
+    }
+}
